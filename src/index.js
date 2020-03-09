f754f7c370d51cbfd8f03dc09c9bd8f123d3ef20
Artierm <artyom.rus.ct100@mail.ru>
2020-03-09 23:35:34 +0300

    do it

--- src/index.js
+++ src/index.js
@@ -38,8 +38,48 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    expr.split("");
+    const array_size = 10;
+    let sliced_expr = [];
+    let final_string = "";
+    for (let i = 0; i <expr.length; i += array_size) {
+        sliced_expr.push(expr.slice(i, i + array_size));
+    }
+
+    for (let i = 0; i < sliced_expr.length; i++) {
+        morze_expr = "";
+        for (let j = 0; j < sliced_expr[i].length; j+=2) {
+            if(sliced_expr[i][j]==0 && sliced_expr[i][j]==0)
+            {
+                continue;
+            }
+            if(sliced_expr[i][j]==1 && sliced_expr[i][j+1]==0)
+            {
+                morze_expr += '.';
+            }
+            else
+            if(sliced_expr[i][j]==1 && sliced_expr[i][j+1]==1)
+            {
+                morze_expr += '-';
+            }
+            else
+            if(sliced_expr[i][j]=="*")
+            {
+                morze_expr += ' ';
+                break;
+            }   
+        }   
+        if (morze_expr == " ") {
+            final_string += morze_expr;
+        }
+        else {
+            final_string += MORSE_TABLE[morze_expr];
+        }
+    }
+    return final_string;
 }
+
+
 
 module.exports = {
     decode
